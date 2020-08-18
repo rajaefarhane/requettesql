6e31bd8edc2170168db3cb3ab1d8f56488ffadc4
user.name <r.farhane@codeur.online>
2020-08-18 09:29:16 +0200

    update exercice

--- index.php
+++ index.php
@@ -56,75 +56,75 @@ include 'connection.php';
 
 
 <h1>1) Afficher tous les gens dont le nom est « Palmer »</h1>
-<h3>"SELECT * FROM `datas` WHERE `last_name`='Palmer'"</h3>
+<h3 class="couleur">"SELECT * FROM `datas` WHERE `last_name`='Palmer'"</h3>
     <?php
     include 'afficherpalmer.php';
     ?>
 
 
     <h1>2) Afficher toutes les femmes (et uniquement les femmes)</h1>
-    <h3>" SELECT * FROM `datas` WHERE `gender`='Female'"</h3>
+    <h3 class="couleur">" SELECT * FROM `datas` WHERE `gender`='Female'"</h3>
     <a href="female.php"><input type="button" value="afficher" class="bouton"></a>
 
 <br><br>
 
 <h1>3) Afficher tous les états dont la lettre commence par N</h1>
-<h3>" SELECT * FROM `datas` WHERE `country_code` LIKE 'N%'"</h3>
+<h3 class="couleur">" SELECT * FROM `datas` WHERE `country_code` LIKE 'N%'"</h3>
     <a href="commenceparn.php"><input type="button" value="afficher" class="bouton"></a>
 
 <br><br>
 <h1>4) Afficher tous les emails qui contiennent le mot « google »</h1>
-<h3>" SELECT * FROM `datas` WHERE `email` LIKE '%google%'"</h3>
+<h3 class="couleur">" SELECT * FROM `datas` WHERE `email` LIKE '%google%'"</h3>
     <a href="emailgoogle.php"><input type="button" value="afficher" class="bouton"></a>
 
 <br><br>
 <h1>5) Insérer une nouvelle personne dans la liste</h1>
-<h3>"INSERT INTO `datas`(`id`, `first_name`, `last_name`, `email`, `gender`, `ip_address`, `birth_date`, `zip_code`, `avatar_url`, `state_code`, `country_code`) VALUES ('1003','farhan','rajae','rajae19beddi@gmail.com','','','','','','','')"</h3>
+<h3 class="couleur">"INSERT INTO `datas`(`id`, `first_name`, `last_name`, `email`, `gender`, `ip_address`, `birth_date`, `zip_code`, `avatar_url`, `state_code`, `country_code`) VALUES ('1003','farhan','rajae','rajae19beddi@gmail.com','','','','','','','')"</h3>
     <a href="insert.php"><input type="button" value="afficher" class="bouton"></a>
 
 <br><br>
 <h1>6) Mettre à jour l'adresse email de la nouvelle personne</h1>
-<h3>"UPDATE `datas` SET `email`='na@gmail.com' WHERE `id` ='1003'"</h3>
+<h3 class="couleur">"UPDATE `datas` SET `email`='na@gmail.com' WHERE `id` ='1003'"</h3>
     <a href="update.php"><input type="button" value="afficher" class="bouton"></a>
 
 <br><br>
 <h1>7) Supprimer la nouvelle personne</h1>
-<h3>"DELETE FROM `datas` WHERE `id`='1002' "</h3>
+<h3 class="couleur">"DELETE FROM `datas` WHERE `id`='1002' "</h3>
     <a href="delet.php"><input type="button" value="afficher" class="bouton"></a>
 
 <br><br>
 <h1>8) Afficher le nombre de femmes </h1>
-<h3>"SELECT COUNT(*) FROM `datas` WHERE `gender`= 'female' "</h3>
+<h3 class="couleur">"SELECT COUNT(*) FROM `datas` WHERE `gender`= 'female' "</h3>
     <a href="countfemale.php"><input type="button" value="afficher" class="bouton"></a>
 
 <br><br>
 <h1>8) Afficher le nombre de hommes </h1>
-<h3>"SELECT COUNT(*) FROM `datas` WHERE `gender`= 'male'"</h3>
+<h3 class="couleur">"SELECT COUNT(*) FROM `datas` WHERE `gender`= 'male'"</h3>
     <a href="countmal.php"><input type="button" value="afficher" class="bouton"></a>
 
 <br><br>
 <h1>9)  afficher l'age</h1>
-<h3>"SELECT ( 2020 - (SUBSTR(`birth_date`,7))) AS agefinale
+<h3 class="couleur">"SELECT ( 2020 - (SUBSTR(`birth_date`,7))) AS agefinale
 FROM datas  "</h3>
     <a href="age.php"><input type="button" value="afficher" class="bouton"></a>
 
 <br><br>
 <h1>9) la moyenne d’âge des femmes  </h1>
-<h3>"SELECT  ROUND(AVG(( 2020 - (SUBSTR(`birth_date`,7))))) AS agefinale
+<h3 class="couleur">"SELECT  ROUND(AVG(( 2020 - (SUBSTR(`birth_date`,7))))) AS agefinale
 FROM datas
 WHERE `gender`='female' "</h3>
     <a href="moyenneagefemale.php"><input type="button" value="afficher" class="bouton"></a>
 
 <br><br>
 <h1>9) la moyenne d’âge des hommes </h1>
-<h3>"SELECT  ROUND(AVG(( 2020 - (SUBSTR(`birth_date`,7))))) AS agefinale
+<h3 class="couleur">"SELECT  ROUND(AVG(( 2020 - (SUBSTR(`birth_date`,7))))) AS agefinale
 FROM datas
 WHERE `gender`='male' "</h3>
     <a href="moyenneagemale.php"><input type="button" value="afficher" class="bouton"></a>
 
 <br><br>
 <h1>9) Afficher la répartition par État et trier en fonction de la répartition par ordre croissant </h1>
-<h3>"SELECT `country_code`, SUM(id) AS sum FROM `datas` GROUP BY `country_code` ORDER BY sum ASC"</h3>
+<h3 class="couleur">"SELECT `country_code`, SUM(id) AS sum FROM `datas` GROUP BY `country_code` ORDER BY sum ASC"</h3>
     <a href="question10.php"><input type="button" value="afficher" class="bouton"></a>
     <br>
     <div id="footer">
